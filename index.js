689e6ceb8d09bed47de0eaadee71ea8521d7e08e
Malevolenc <magadiamatthew13@gmail.com>
2024-12-03 21:47:45 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const button = document.getElementById("skibidi_button");
 const counter = document.getElementById("counter");
+const button1 = document.getElementById("button1");
 const upgrade1 = document.getElementById("upgrade1");
+const cps = document.getElementById("click_per_second");
 
 let value = 0;
 let increment = 1;
 let cost = 10;
+let upgrade = 1
 
 
 button.onclick = function(){
     value += increment
-    counter.textContent = value.toFixed(1)
+    counter.textContent = `${value.toFixed(1)} Skibidi Bucks`;
 }
 
 
-upgrade1.onclick = function(){
+button1.onclick = function(){
     if(value < cost){
         window.alert(`You need ${(cost-value).toFixed(1)} Skibidi Bucks to upgrade!`);
     }
@@ -21,10 +24,13 @@ upgrade1.onclick = function(){
         value-=cost;
         increment++;
         cost += cost * 0.5;
+        upgrade++
     }
 
-    upgrade1.textContent= `Upgrade: ${cost.toFixed(1)} Skibidi Bucks`
-    counter.textContent = value.toFixed(1);
+    button1.textContent = `${cost.toFixed(1)} Skibidi Bucks`
+    upgrade1.textContent = `Upgrade Toilet Touch | LVL: ${upgrade}`
+    counter.textContent = `${value.toFixed(1)} Skibidi Bucks`;
+    cps.textContent = `${increment} Skibidi Bucks Per Click`
 
 }
 
